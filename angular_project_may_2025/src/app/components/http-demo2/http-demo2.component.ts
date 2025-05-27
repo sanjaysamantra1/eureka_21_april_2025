a2289393fd92e1711ef9d9e66a5a2f1c2c33573c
Sanjay Samantra <sanjaysamantra1@gmail.com>
2025-05-27 21:16:34 +0530

    http interceptors

--- angular_project_may_2025/src/app/components/http-demo2/http-demo2.component.ts
+++ angular_project_may_2025/src/app/components/http-demo2/http-demo2.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { EmployeeService } from '../../services/employee.service';
 import { Employee } from '../../models/employee-model';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-http-demo2',
@@ -10,12 +11,21 @@ import { Employee } from '../../models/employee-model';
 })
 export class HttpDemo2Component {
   employees: Employee[] = [];
-  constructor(private employeeService: EmployeeService) {
+  constructor(private employeeService: EmployeeService, private httpClient: HttpClient) {
   }
   ngOnInit() {
+    this.fetchEmployees();
+    this.fetchUsers();
+  }
+  fetchEmployees() {
     this.employeeService.getEmployees().subscribe((response: Employee[]) => {
       console.log(response);
       this.employees = response;
+    })
+  }
+  fetchUsers() {
+    this.httpClient.get('https://jsonplaceholder.typicode.com/users').subscribe(response => {
+      console.log(response)
     })
   }
 }
